ac61a6aba0d36b4f9f8d54f1e9762f3a3f8a6e51
Maorb23 <maorblumberg@gmail.com>
2026-01-08 10:08:19 +0200

    fixed name

---
+++ scripts/utils/utils_tokenizer.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from transformers import AutoTokenizer
+
+
+def assert_tokenizer_consistency(model_id_1, model_id_2):
+    identical_tokenizers = (
+            AutoTokenizer.from_pretrained(model_id_1).vocab
+            == AutoTokenizer.from_pretrained(model_id_2).vocab
+    )
+    if not identical_tokenizers:
+        raise ValueError(f"Tokenizers are not identical for {model_id_1} and {model_id_2}.")
